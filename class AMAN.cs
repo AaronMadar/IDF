75d0023bae376073144ddf3e4b67640a5c8cad36
Aaron <aaronmadar2655@gmail.com>
2025-05-22 16:13:24 +0300

    Class Aman + CreateReportMsg()

---
+++ class AMAN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectClassIDF
+{
+    public class AMAN
+    {
+        public static void CreateReportMsg(Terrorist terrorist, string place, DateTime specificDate)
+        {
+            Console.WriteLine($"{terrorist.Name} is at {place}, {specificDate:dd/MM/yyyy HH:mm}");
+        }
+    }
+}
