a4e4d7c38c6edc155dcdf74ed899156ab820f827
Aaron <aaronmadar2655@gmail.com>
2025-05-25 15:10:15 +0300

    osafat DisplayMostReportedTarget()

--- class AMAN.cs
+++ class AMAN.cs
@@ -7,6 +7,8 @@ namespace ProjectClassIDF
     {
         private static Dictionary<string, int> terroristReported = new Dictionary<string, int>();
 
+
+
         public static void CreateReportMsg(Terrorist terrorist, string place, DateTime specificDate)
         {
             Console.WriteLine($"{terrorist.Name} is at {place}, {specificDate:dd/MM/yyyy HH:mm}");
@@ -16,5 +18,28 @@ namespace ProjectClassIDF
             else
                 terroristReported[terrorist.Name] = 1;
         }
+
+
+
+
+        public static void DisplayMostReportedTarget()
+        {
+
+            string mostReported = null;
+            int maxReports = -1;
+
+            foreach (var entry in terroristReported)
+            {
+                if (entry.Value > maxReports)
+                {
+                    mostReported = entry.Key.Name;
+                    maxReports = entry.Value;
+                }
+            }
+
+            Console.WriteLine($"The terrorist the most reported is  {mostReported} with {maxReports} reports.");
+        }
+
+
     }
 }
