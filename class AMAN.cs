3b440bd8ddc72c0686e5da06b30aaf19de2fabf3
Aaron <aaronmadar2655@gmail.com>
2025-05-25 12:48:56 +0300

    creation Dict TerroristReported + incrementation for all report

--- class AMAN.cs
+++ class AMAN.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProjectClassIDF
 {
     public class AMAN
     {
+        private static Dictionary<string, int> terroristReported = new Dictionary<string, int>();
+
         public static void CreateReportMsg(Terrorist terrorist, string place, DateTime specificDate)
         {
             Console.WriteLine($"{terrorist.Name} is at {place}, {specificDate:dd/MM/yyyy HH:mm}");
+
+            if (terroristReported.ContainsKey(terrorist.Name))
+                terroristReported[terrorist.Name]++;
+            else
+                terroristReported[terrorist.Name] = 1;
         }
     }
 }
